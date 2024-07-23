8cc68533207ef5da7b9959431e85b77a051ae604
Swarit Pandey <swarit@accuknox.com>
2024-07-23 10:53:02 +0000

    feat: scan

---
+++ src/main/index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const https = require('https')
+const fs = require('fs')
+
+async function getLatestKubeArmorVersion() {
+    
+}
