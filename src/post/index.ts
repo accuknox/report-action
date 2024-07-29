423daa48f615f2bdf4830fb2c09ec287a1378146
swarit <swarit@accuknox.com>
2024-07-29 21:53:14 +0530

    fix: lint issues (#9)
    Signed-off-by: Swarit Pandey <swarit@accuknox.com>

--- src/post/index.ts
+++ src/post/index.ts
@@ -1,12 +1,12 @@
-import * as core from "@actions/core";
 import * as fs from "node:fs";
 import * as path from "node:path";
+import * as core from "@actions/core";
 import {
-	log,
-	getPidFilePath,
-	getOutputDir,
-	IS_GITHUB_ACTIONS,
 	ENCODING,
+	IS_GITHUB_ACTIONS,
+	getOutputDir,
+	getPidFilePath,
+	log,
 } from "@common/common";
 
 const NETWORK_EVENTS_PREFIX = "knoxctl_scan_network_events_md_";
